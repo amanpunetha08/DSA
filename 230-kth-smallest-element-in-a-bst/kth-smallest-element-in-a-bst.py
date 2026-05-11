7aec65e1ca3de4984751203870c0c2ead14f526b
Aman Punetha <122922091+amanpunetha08@users.noreply.github.com>
2026-05-11 08:09:50 +0530

    Time: 4 ms (14.05%) | Memory: 22 MB (87.00%) - LeetSync

--- 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.py
+++ 230-kth-smallest-element-in-a-bst/kth-smallest-element-in-a-bst.py
@@ -5,15 +5,15 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def print(self,root,ans):
+    def print(self,root,ans,k):
         if not root:
             return
-        
-        self.print(root.left,ans)
-        ans.append(root.val)
-        self.print(root.right,ans)
+        if len(ans) <k:
+            self.print(root.left,ans,k)
+            ans.append(root.val)
+            self.print(root.right,ans,k)
     def kthSmallest(self, root: Optional[TreeNode], k: int) -> int:
         ans = []
-        self.print(root,ans)
+        self.print(root,ans,k)
         return ans[k-1]
         
