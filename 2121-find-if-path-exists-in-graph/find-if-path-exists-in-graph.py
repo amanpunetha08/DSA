d9c47e887726d848c48085933464ac9d8f3dfda4
Aman Punetha <122922091+amanpunetha08@users.noreply.github.com>
2026-06-17 07:56:35 +0530

    Time: 229 ms (91.62%) | Memory: 83.1 MB (98.03%) - LeetSync

--- 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.py
+++ 2121-find-if-path-exists-in-graph/find-if-path-exists-in-graph.py
@@ -1,21 +1,50 @@
-from collections import deque
+# from collections import deque
+# class Solution:
+#     def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
+#         graph = [[] for _ in range(n)]
+#         for u,v in edges:
+#             graph[u].append(v)
+#             graph[v].append(u)
+        
+#         visited = {source}
+#         q = deque([source])
+#         while q:
+#             node = q.popleft()
+#             if node == destination:
+#                 return True
+            
+#             for nei in graph[node]:
+#                 if nei not in visited:
+#                     visited.add(nei)
+#                     q.append(nei)
+#         return False
+
+class DSU:
+    def __init__(self,n):
+        self.parent = list(range(n))
+        self.rank = [0] * n
+    
+    def find(self,x):
+        if self.parent[x] != x:
+            self.parent[x] = self.find(self.parent[x])
+        return self.parent[x]
+    
+    def union(self,x,y):
+        px,py = self.find(x), self.find(y)
+        if px == py:
+            return False
+        if self.rank[px] < self.rank[py]:
+            px,py = py,px
+        self.parent[py] = px
+        if self.rank[px] == self.rank[py]:
+            self.rank[px] +=1
+        return True
+
 class Solution:
     def validPath(self, n: int, edges: List[List[int]], source: int, destination: int) -> bool:
-        graph = [[] for _ in range(n)]
+        dsu = DSU(n)
+
         for u,v in edges:
-            graph[u].append(v)
-            graph[v].append(u)
-        
-        visited = {source}
-        q = deque([source])
-        while q:
-            node = q.popleft()
-            if node == destination:
-                return True
-            
-            for nei in graph[node]:
-                if nei not in visited:
-                    visited.add(nei)
-                    q.append(nei)
-        return False
+            dsu.union(u,v)
+        return dsu.find(source) == dsu.find(destination)
         
